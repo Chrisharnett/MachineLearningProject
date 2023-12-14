ec48d1c824a48daa31e5412bdf78aae8e2078595
Bard of Code <harnettmusic@gmail.com>
2023-12-13 20:59:54 -0330

    Final product

--- forms.py
+++ forms.py
@@ -7,17 +7,17 @@ from wtforms.validators import DataRequired, NumberRange
 
 
 class VehicleInformationForm(FlaskForm):
-    carCylinders = IntegerField("Cylinders:", validators=[DataRequired()])
-    carHorsepower = DecimalField("Horsepower:", places=2, rounding=decimal.ROUND_UP, validators=[DataRequired()])
-    carWeight = IntegerField("Weight:", validators=[DataRequired()])
-    carYear = IntegerField("Year:", validators=[DataRequired()])
+    carCylinders = IntegerField("Cylinders:", validators=[DataRequired(), NumberRange(min=3, max=12)])
+    carHorsepower = DecimalField("Horsepower:", places=2, rounding=decimal.ROUND_UP, validators=[DataRequired(), NumberRange(min=40, max=200)])
+    carWeight = IntegerField("Weight:", validators=[DataRequired(), NumberRange(min=1500, max=5200)])
+    carYear = IntegerField("Year:", validators=[DataRequired(), NumberRange(min=1950, max=2024)])
     carOrigin = SelectField("Origin:", choices=["USA", "Japan", "Europe"], validators=[DataRequired()])
 
     submit = SubmitField("Submit")
 
 class PersonalDataForm(FlaskForm):
-    personAge = IntegerField("Age:", validators=[DataRequired(), NumberRange(min=0, max=120)])
-    personBMI = DecimalField("BMI:", places=2, rounding=decimal.ROUND_UP, validators=[DataRequired(), NumberRange(min=0, max=70)])
-    personGlucose = IntegerField("Glucose:", validators=[DataRequired(), NumberRange(min=0, max=200)])
+    personAge = IntegerField("Age:", validators=[DataRequired(), NumberRange(min=1, max=120)])
+    personBMI = DecimalField("BMI:", places=2, rounding=decimal.ROUND_UP, validators=[DataRequired(), NumberRange(min=1, max=70)])
+    personGlucose = IntegerField("Glucose:", validators=[DataRequired(), NumberRange(min=1, max=200)])
 
     submit = SubmitField("Submit")
--- static/js/main.js
+++ static/js/main.js
@@ -5,16 +5,49 @@ $( () => {
     const bmiValueDisplay = document.getElementById('bmiValue');
     const glucoseSlider = document.getElementById('personGlucose');
     const glucoseValueDisplay = document.getElementById('glucoseValue');
+    const cylinderSlider = document.getElementById('carCylinders');
+    const cylinderValueDisplay = document.getElementById('cylindersValue');
+    const horsepowerSlider = document.getElementById('carHorsepower');
+    const horsepowerValueDisplay = document.getElementById('horsepowerValue');
+    const weightSlider = document.getElementById('carWeight');
+    const weightValueDisplay = document.getElementById('weightValue');
+    const carYearSlider = document.getElementById('carYear');
+    const carYearValueDisplay = document.getElementById('carYearValue');
 
-    bmiValueDisplay.textContent = parseFloat(bmiSlider.value).toFixed(1);
-    glucoseValueDisplay.textContent = parseInt(glucoseSlider.value);
+    if (bmiSlider && glucoseSlider) {
+        bmiValueDisplay.textContent = parseFloat(bmiSlider.value).toFixed(1);
+        glucoseValueDisplay.textContent = parseInt(glucoseSlider.value);
 
-    bmiSlider.oninput = function() {
-        const roundedValue = parseFloat(bmiSlider.value).toFixed(1);
-        bmiValueDisplay.textContent = roundedValue;
+        bmiSlider.oninput = function() {
+            const roundedValue = parseFloat(bmiSlider.value).toFixed(1);
+            bmiValueDisplay.textContent = roundedValue;
+        }
+
+        glucoseSlider.oninput = function() {
+            glucoseValueDisplay.textContent = parseInt(glucoseSlider.value);
+        }
     }
 
-    glucoseSlider.oninput = function() {
-        glucoseValueDisplay.textContent = parseInt(glucoseSlider.value);
+    else if (cylinderSlider && horsepowerSlider && weightSlider && carYearSlider){
+        cylinderValueDisplay.textContent = parseInt(cylinderSlider.value);
+        horsepowerValueDisplay.textContent = parseFloat(horsepowerSlider.value).toFixed(2);
+        weightValueDisplay.textContent = parseInt(weightSlider.value);
+        carYearValueDisplay.textContent = parseInt(carYearSlider.value);
+
+        cylinderSlider.oninput = function() {
+            cylinderValueDisplay.textContent = parseInt(cylinderSlider.value);
+        }
+
+        horsepowerSlider.oninput = function() {
+            horsepowerValueDisplay.textContent = parseFloat(horsepowerSlider.value).toFixed(1);
+        }
+
+        weightSlider.oninput = function()   {
+            weightValueDisplay.textContent = parseInt(weightSlider.value);
+        }
+
+        carYearSlider.oninput = function() {
+            carYearValueDisplay.textContent = parseInt(carYearSlider.value);
+        }
     }
 });
